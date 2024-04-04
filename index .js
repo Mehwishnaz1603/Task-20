9253957fcd5b039f0a6bf0125cc78bff2a87e5ba
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-04 21:14:25 +0500

    Task no 20

---
+++ index .js
@@ -0,0 +1,9 @@
+////Task # 20 ///
+//Think of something you could store in a array. For example, you could make a list of mountains, 
+//rivers, countries, cities, languages, or anything else you’d like. Write a program that creates 
+//a list containing these items.
+////Countries///
+let countries = ["Switzerland", "Japan", "London", "Norway", "Dubai", "Turkey"];
+console.log(`I would like to visit: ${countries}`);
+export {};
+///Mountains///
---
+++ index .ts
@@ -0,0 +1,18 @@
+    ////Task # 20 ///
+
+//Think of something you could store in a array. For example, you could make a list of mountains, 
+//rivers, countries, cities, languages, or anything else you’d like. Write a program that creates 
+//a list containing these items.
+                    ////Countries///
+let countries: string[] = ["Switzerland", "Japan", "London", "Norway","Dubai","Turkey"]
+console.log(`I would like to visit: ${countries}` )
+                    ///Mountains///
+let mountains: string[] = ["K2", "Himalaya", "Hindu Kash", "Salt Range"]
+console.log(`I would like to See this: ${mountains}` )
+                                    ///Cities///
+let cities: string[] = ["Karachi", "Lahore", "Islamabad", "Sawat","kagan"]
+console.log(`I would like go for vacations: ${cities}` )
+                                    ///Languages///
+let languages: string[] = ["English", "Arabic", "Chinese", "Hindi"]
+console.log(`I would like to Speak: ${languages}` )
+                                                  
